2aa8b4fb41152325c523535592214f6b56504c7f
Oliver Henning <oli.henning@gmail.com>
2023-10-15 00:49:02 +0200

    steps towards strong encryption (AES)

--- src/main/java/ch/oli/zipAnalyzer/AesDecrypt.java
+++ src/main/java/ch/oli/zipAnalyzer/AesDecrypt.java
@@ -11,6 +11,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.Provider;
 import java.security.Security;
+import java.util.Arrays;
 import java.util.HexFormat;
 
 public class AesDecrypt {
@@ -27,7 +28,7 @@ public class AesDecrypt {
         String password = "GEHEIM";
         InputStream is = Files.newInputStream(path);
         is.skip(52);                       // hard coded for now
-        final int compressedSize = 174613; // hard coded for now, too: found in from local header
+        int compressedSize = 174613; // hard coded for now, too: found in from local header
 
         // see https://www.winzip.com/en/support/aes-encryption/#file-format1
         int sizeSalt       = 16; // for AES256: 128 bits = 16 bytes
@@ -43,29 +44,36 @@ public class AesDecrypt {
         // see also https://github.com/lclevy/unarcrypto/blob/master/ziparchive.py
         int keySize = 32; // 32 bytes = 256 bits for AES256
         SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-        PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 1000, (2 * keySize + 2) * 8);
+        PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 1000, (2 * keySize + sizePwVerifier) * 8);
         byte[] derivedKeys = skf.generateSecret(spec).getEncoded();
         ByteArrayInputStream bais = new ByteArrayInputStream(derivedKeys);
-        byte[] aesKey     = bais.readNBytes(keySize); // the "encryption key"
-        byte[] macKey     = bais.readNBytes(keySize); // the "authentication key"
-        byte[] pwVerifier = bais.readNBytes(2);       // the "password verification value"
+        byte[] aesKey     = bais.readNBytes(keySize       ); // the "encryption key"
+        byte[] macKey     = bais.readNBytes(keySize       ); // the "authentication key"
+        byte[] pwVerifier = bais.readNBytes(sizePwVerifier); // the "password verification value"
 
-        // check password: those 16 bits must be identical (i.e.: chance 1:65536 to have a wrong password and to decrypt to rubbish)
+        // check password: those 16 bits must be identical
+        // --> chance 1:65536 to accept wrong password - then we fail with check of HMAC-SHA1-80
         System.out.println("PW-Verification in header    : " + HexFormat.of().formatHex(pwVerifierInHeader));
         System.out.println("PW-Verification from password: " + HexFormat.of().formatHex(pwVerifier));
+        if (Arrays.compare(pwVerifierInHeader, pwVerifier) != 0) {
+            throw new RuntimeException("wrong passwort");
+        }
         System.out.println("---------------------------------------------------------------");
 
         Mac mac = Mac.getInstance("HmacSHA1");
         mac.init(new SecretKeySpec(macKey, "HmacSHA1"));
-        byte[] hmacOfEncryptedData = mac.doFinal(data);
-        // check HMAC: first 10 bytes (80 bits) bits must be identical as it's a HMAC-SHA1-80
+        byte[] hmacOfEncryptedData = mac.doFinal(data); // HMAC is calculated over _encrypted_ data
+        // ZIP-AES uses HMAC-SHA1-80 - so compare only first 80 bits (=10 bytes)
         System.out.println("HMAC in header     : " + HexFormat.of().formatHex(hmacInHeader));
         System.out.println("HMAC encrypted data: " + HexFormat.of().formatHex(hmacOfEncryptedData));
+        if (Arrays.compare(hmacInHeader, 0, 10, hmacOfEncryptedData, 0, 10) != 0) {
+            throw new RuntimeException("authentication code mismatch");
+        }
         System.out.println("---------------------------------------------------------------");
 
         // ZIP uses a "Little"-Endian counter-mode (CTR)
-        // Java's CounterMode is hard coded to "Big"-endian (see com.sun.crypto.provider.CounterMode#increment)
-        // --> we can't use "AES/CTR/NoPadding" and we need to do our own CTR-Mode
+        // Unfortunately, Java's CounterMode is hard coded to "Big"-endian (see com.sun.crypto.provider.CounterMode#increment)
+        // --> we can't use "AES/CTR/NoPadding" and we need to do our own CTR-Mode. Though, this is easy
         Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding");
         cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(aesKey, "AES"));
         byte[] counter          = new byte[16];
